416ca93cb465ce3e6ca6e16de44a0b3f35ccb0a0
Armoteo <armoteo@ukr.net>
2020-01-18 00:59:54 +0200

    Updates

---
+++ precache-manifest.cf3b0d7b7492fe6346da1c6c3d81121e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c0e1f2a57285649aae67eee74c084c4",
+    "url": "/react-clock/index.html"
+  },
+  {
+    "revision": "a8168e865286ab17bd71",
+    "url": "/react-clock/static/css/main.11fdf51a.chunk.css"
+  },
+  {
+    "revision": "754aafaccffe3f638cbd",
+    "url": "/react-clock/static/js/2.02fd07fc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-clock/static/js/2.02fd07fc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a8168e865286ab17bd71",
+    "url": "/react-clock/static/js/main.7fa7f5d4.chunk.js"
+  },
+  {
+    "revision": "d217ae0ff263e74110aa",
+    "url": "/react-clock/static/js/runtime-main.ec5b3200.js"
+  }
+]);
